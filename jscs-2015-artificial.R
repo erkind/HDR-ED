fbd1c082f95791f28cd69b053de2b2c22451866a
erkind <erkindoglu@gmail.com>
2025-08-05 15:44:45 +0200

    Updates
    Updates to R code

--- jscs-2015-artificial.R
+++ jscs-2015-artificial.R
@@ -2,36 +2,27 @@
 # linear regression using CGA's", J of Statistical Computation and
 # Simulation
 #
-# Importing the data
-
-# source(paste("http://raw.githubusercontent.com/erkind/",
-#              "eiv-coe/main/g-f-HM-Zmat.R",
-#              sep = ""))
-
-# Importing from GitHub
+# Importing data
 data <- read.csv(file = paste("http://raw.githubusercontent.com/",
                               "erkind/hdr-ed/main/",
                               "jscs-2015-artificial.csv", sep = ""),
                  header = TRUE, sep = ";")
-#
-# # Importing from local drive
-# data <- read.csv(file = paste("C:/Users/68596/Documents/docs/",
-#                               "data/eiv/jscs-2015-artificial.csv", sep = ""),
-#                        header = TRUE, sep = ";")
-head(data)
-dim(data)
-#
-# Description of the data
-# Description goes here...
+attach(data)
 #
 # Importing "eive" library
 library(eive)
 #
-# Fit OLS regression of y on clean variables W1 and W2
-fit.ols <- eive.cga(dirtyx = X1, otherx = W2,
-                    y = y, numdummies = 10)$ols
+# OLS regression of Y on (true) W1 and (true) W2
+fit.ols <- lm(formula = y ~ W1 + W2)
+#
+# OLS regression of Y on (eiv) X1 and (true) W2
+fit.eiv <- lm(formula = y ~ X1 + W2)
+# 
+# CGA regression of Y on (cga) X1 and (true) W2
+fit.cga <- eive.cga(dirtyx = X1, otherx = W2, y = y)$eive
+#
+# Summary output
+t(summary(fit.ols)$coefficients[,c(1,3)])
+t(summary(fit.eiv)$coefficients[,c(1,3)])
+t(summary(fit.cga)$coefficients[,c(1,3)])
 
-# Fit OLS regression of y on dirty X1 and clean W2
-
-
-# Fit CGA regression of y on filtered X1 and clean W2
